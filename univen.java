a8a1e1183db896879d67ddfa17b6445f00b7e271
25043808 <25043808@mvula.univen.ac.za>
2025-04-19 09:44:11 -0700

    Add files via upload

---
+++ univen.java
@@ -0,0 +1,9 @@
+
+public class univen {
+   public static void welcome(){
+       System.out.print("welcome to univen");}
+       public static void main (String []args){
+           univen.welcome();
+       }
+}
+   
